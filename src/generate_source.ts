f6595b4e91c75cefe232c6e0767a8c3fdacac1d8
Dan Baranov <ICDan.Baranov@amwell.com>
2023-06-13 23:34:01 +0400

    add generator stub

---
+++ src/generate_source.ts
@@ -0,0 +1,52 @@
+import fs from "fs";
+import path from "path";
+
+interface FileObject {
+  file: string;
+  content: string;
+  entry?: boolean;
+}
+
+interface Config {
+  destination: string;
+  bundleName: string;
+  files: FileObject[];
+}
+
+// Read the JSON file
+const jsonFilePath = "./input.json"; // Replace with the actual file path
+const jsonData = fs.readFileSync(jsonFilePath, "utf8");
+const config: Config = JSON.parse(jsonData);
+
+// Create the output directory if it doesn't exist
+const outputDir = path.resolve(config.destination);
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
+}
+
+// Iterate over each file object in the JSON
+config.files.forEach((fileObj) => {
+  const filePath = path.resolve(fileObj.file);
+  const fileContent = fileObj.content;
+
+  // Create the source file
+  fs.writeFileSync(filePath, fileContent);
+
+  // If the entry flag is set, create or update the bundle file
+  if (fileObj.entry) {
+    const bundleFilePath = path.resolve(config.destination, config.bundleName);
+    const existingBundleContent = fs.existsSync(bundleFilePath)
+      ? fs.readFileSync(bundleFilePath, "utf8")
+      : "";
+
+    // Append the file content to the existing bundle file
+    fs.writeFileSync(
+      bundleFilePath,
+      existingBundleContent + "\n" + fileContent
+    );
+  }
+
+  console.log(`Created file: ${filePath}`);
+});
+
+console.log("Source files created successfully.");
